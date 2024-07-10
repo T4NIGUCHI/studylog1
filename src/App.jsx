e4d173f3edf1458bf0c96c54bc5b53f72203754b
T4niguchi <tech@noizless.co.jp>
2024-07-10 17:51:05 +0900

    before_FireBase_deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
+
+// Firebase を初期化
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const analytics = getAnalytics(app);
+console.log('Firebase Config:', firebaseConfig);
+// ここから React のコード
 import { useState, useEffect } from "react";
 import "./styles.css";
 import { createClient } from '@supabase/supabase-js';
@@ -72,6 +92,21 @@ export const App = () => {
     }
   };
 
+  const handleDeleteRecord = async (record) => {
+    const { error } = await supabase
+      .from('study-record')
+      .delete()
+      .eq('title', record.title)
+      .eq('time', record.time);
+
+    if (error) {
+      console.error("Error deleting record:", error);
+    } else {
+      const updatedRecords = records.filter(item => item !== record);
+      setRecords(updatedRecords);
+    }
+  };
+
   const sumTime = records.reduce((sum, record) => sum + parseInt(record.time, 10), 0);
 
   if (isLoading) {
@@ -101,6 +136,7 @@ export const App = () => {
             {records.map((record, index) => (
               <li key={index}>
                 {record.title} {record.time}時間
+                <button onClick={() => handleDeleteRecord(record)}>削除</button>
               </li>
             ))}
           </ul>
